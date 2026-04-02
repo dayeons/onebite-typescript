1af5aed50048f7e80e141a2702fe6f7cf831cb1e
dayeons <dy7326@gmail.com>
2026-04-02 20:49:21 +0900

    section9

---
+++ section9/src/chapter1.ts
@@ -0,0 +1,64 @@
+/**
+ * 분산적인 조건부 타입
+ */
+// type StringNumberSwitch<T> = [T] extends [number] ? string : number; -> [] 넣으면 분산 방지 한다. 결과 number
+type StringNumberSwitch<T> = T extends number ? string : number;
+
+let a: StringNumberSwitch<number>;
+let b: StringNumberSwitch<string>;
+
+let c: StringNumberSwitch<number | string>;
+// StringNumberSwitch<number> |
+// StringNumberSwitch<string>
+
+let d: StringNumberSwitch<boolean | number | string>;
+// 1단계 StringNumberSwitch<boolean> |
+// StringNumberSwitch<number> |
+// StringNumberSwitch<string>
+
+// 2단계
+// number |
+// string |
+// number
+
+// 결과
+// number | string
+
+/**
+ * 실용적인 예제
+ */
+
+type Exclude<T, U> = T extends U ? never : T;
+
+type A = Exclude<number | string | boolean, string>;
+// 1단계
+// Exclude<number, string> |
+// Exclude<string, string> |
+// Exclude<boolean, string>
+
+// 2단계
+// number |
+// never |
+// boolean
+
+// 결과
+// number | never | boolean
+// (never 공집합) -> 사라짐
+// ∴ number | boolean
+
+// type Extract<T, U> = any;
+type Extract<T, U> = T extends U ? T : never;
+
+type B = Extract<number | string | boolean, string>;
+// 1단계
+// Extract<number, string> |
+// Extract<string, string> |
+// Extract<boolean, string>
+
+// 2단계
+// never |
+// string |
+// never
+
+// 결과
+// string
