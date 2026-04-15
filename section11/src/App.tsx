544d4c6aa782d2d574f24592b700993cccbab297
dayeons <dy7326@gmail.com>
2026-04-15 23:19:40 +0900

    feat: useReducer

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,19 +1,49 @@
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useReducer } from "react";
 import { Todo } from "./types";
 import Editor from "./components/Editor";
 import TodoItem from "./components/TodoItem";
 
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "DELETE": {
+      return state.filter(it => it.id !== action.id);
+    }
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([...todos, { id: idRef.current++, content: text }]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        content: text,
+      },
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
   };
 
   useEffect(() => {
